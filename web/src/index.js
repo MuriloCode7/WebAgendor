5b0e96102ff20a2e8051f0f8165898c9792e5134
MuriloMagal <murilomagalhaes60@gmail.com>
2023-10-17 13:38:26 -0300

    sync

--- web/src/index.js
+++ web/src/index.js
@@ -5,6 +5,8 @@ import Routes from './routes';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Routes />
+    < Routes />
   </React.StrictMode>
 );
+
+    
